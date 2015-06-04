9a21984d3e334b61f95884f9b7456bb7dbc6a943
Andrew Bernstein <andrewbbernstein@gmail.com>
2015-06-04 13:08:11 -0400

    cnames

--- cnames_active.js
+++ cnames_active.js
@@ -83,6 +83,7 @@ var cnames_active = {
     , "fromdenisvieira": "fromdenisvieira.github.io"
     , "frzr": "pakastin.github.io/frzr"
     , "geekr": "ruanyl.github.io/geekr"
+    , "gendy": "abbernie.github.io/gendy"
     , "ghoullier": "ghoullier.github.io"
     , "goodseller": "goodseller.github.io"
     , "graphics2d": "keyten.github.io/Graphics2D"
